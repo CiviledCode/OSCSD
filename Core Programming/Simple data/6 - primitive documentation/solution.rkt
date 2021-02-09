c305faf63ee55165995feb9959493e4f7178819f
CiviledCode <32830098+CiviledCode@users.noreply.github.com>
2021-02-09 03:32:12 -0800

    Finished unit 6
    Finished unit 6 - primitive documentation and added the solution file

---
+++ Core Programming/Simple data/6 - primitive documentation/solution.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname solution) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Figuring out what primitives do is easy. Right click on the primitive and select the last option to open a simple local file server with the documentation
+
+(require 2htdp/image)
+(triangle 40 "solid" "tan")
